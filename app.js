3f3fb0858271f000cff22ac2f71766c48c2262e7
vic <victordjohnson@gmail.com>
2019-03-09 18:37:26 -0600

    added the pug templating langauge to the project. that's pretty neat

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.send("Way to go, young'n");
+  res.render("index");
 });
 
-app.listen(3000);
+app.get("/hello", (req, res) => {
+  res.send("<h1>hello homey</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("The apllication is running on port 3000");
+});
